f4b194a7bad096fe7e9ff0f2ea91db36c404b007
blackflux <blackflux@unfine.de>
2018-06-10 12:19:19 -0700

    chore: added parser + test

---
+++ src/util/guid.js
@@ -0,0 +1,7 @@
+const s4 = () => {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
+
+module.exports = () => s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
