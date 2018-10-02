5f7b9dc3b2d17b80986db0d6cca86d63310485fd
blackflux <blackflux@unfine.de>
2018-10-01 23:33:13 -0700

    amend: minor rename

--- test/util/compiler.spec.js
+++ test/util/compiler.spec.js
@@ -4,8 +4,8 @@ const compiler = require("./../../src/util/compiler");
 describe("Testing compiler", () => {
   it("Testing traversing", () => {
     const input = ["a.{b,c}.d", "a.{c,e}.f", "a.b.d.g"];
-    const result = compiler.compile(input);
-    expect(result).to.deep.equal({
+    const tower = compiler.compile(input);
+    expect(tower).to.deep.equal({
       a: {
         b: {
           d: {
@@ -22,26 +22,26 @@ describe("Testing compiler", () => {
       }
     });
 
-    expect(compiler.isFinal(result)).to.equal(false);
-    expect(compiler.isFinal(result.a)).to.equal(false);
-    expect(compiler.isFinal(result.a.b)).to.equal(false);
-    expect(compiler.isFinal(result.a.b.d)).to.equal(true);
-    expect(compiler.isFinal(result.a.b.d.g)).to.equal(true);
-    expect(compiler.isFinal(result.a.c)).to.equal(false);
-    expect(compiler.isFinal(result.a.c.d)).to.equal(true);
-    expect(compiler.isFinal(result.a.c.f)).to.equal(true);
-    expect(compiler.isFinal(result.a.e)).to.equal(false);
-    expect(compiler.isFinal(result.a.e.f)).to.equal(true);
+    expect(compiler.isFinal(tower)).to.equal(false);
+    expect(compiler.isFinal(tower.a)).to.equal(false);
+    expect(compiler.isFinal(tower.a.b)).to.equal(false);
+    expect(compiler.isFinal(tower.a.b.d)).to.equal(true);
+    expect(compiler.isFinal(tower.a.b.d.g)).to.equal(true);
+    expect(compiler.isFinal(tower.a.c)).to.equal(false);
+    expect(compiler.isFinal(tower.a.c.d)).to.equal(true);
+    expect(compiler.isFinal(tower.a.c.f)).to.equal(true);
+    expect(compiler.isFinal(tower.a.e)).to.equal(false);
+    expect(compiler.isFinal(tower.a.e.f)).to.equal(true);
 
-    expect(compiler.getNeedles(result)).to.deep.equal(["a.{b,c}.d", "a.{c,e}.f", "a.b.d.g"]);
-    expect(compiler.getNeedles(result.a)).to.deep.equal(["a.{b,c}.d", "a.{c,e}.f", "a.b.d.g"]);
-    expect(compiler.getNeedles(result.a.b)).to.deep.equal(["a.{b,c}.d", "a.b.d.g"]);
-    expect(compiler.getNeedles(result.a.b.d)).to.deep.equal(["a.{b,c}.d", "a.b.d.g"]);
-    expect(compiler.getNeedles(result.a.b.d.g)).to.deep.equal(["a.b.d.g"]);
-    expect(compiler.getNeedles(result.a.c)).to.deep.equal(["a.{b,c}.d", "a.{c,e}.f"]);
-    expect(compiler.getNeedles(result.a.c.d)).to.deep.equal(["a.{b,c}.d"]);
-    expect(compiler.getNeedles(result.a.c.f)).to.deep.equal(["a.{c,e}.f"]);
-    expect(compiler.getNeedles(result.a.e)).to.deep.equal(["a.{c,e}.f"]);
-    expect(compiler.getNeedles(result.a.e.f)).to.deep.equal(["a.{c,e}.f"]);
+    expect(compiler.getNeedles(tower)).to.deep.equal(["a.{b,c}.d", "a.{c,e}.f", "a.b.d.g"]);
+    expect(compiler.getNeedles(tower.a)).to.deep.equal(["a.{b,c}.d", "a.{c,e}.f", "a.b.d.g"]);
+    expect(compiler.getNeedles(tower.a.b)).to.deep.equal(["a.{b,c}.d", "a.b.d.g"]);
+    expect(compiler.getNeedles(tower.a.b.d)).to.deep.equal(["a.{b,c}.d", "a.b.d.g"]);
+    expect(compiler.getNeedles(tower.a.b.d.g)).to.deep.equal(["a.b.d.g"]);
+    expect(compiler.getNeedles(tower.a.c)).to.deep.equal(["a.{b,c}.d", "a.{c,e}.f"]);
+    expect(compiler.getNeedles(tower.a.c.d)).to.deep.equal(["a.{b,c}.d"]);
+    expect(compiler.getNeedles(tower.a.c.f)).to.deep.equal(["a.{c,e}.f"]);
+    expect(compiler.getNeedles(tower.a.e)).to.deep.equal(["a.{c,e}.f"]);
+    expect(compiler.getNeedles(tower.a.e.f)).to.deep.equal(["a.{c,e}.f"]);
   });
 });
