6f82283d19fda7079f6cebb2572593198ffa6561
blackflux <blackflux@unfine.de>
2018-10-15 21:17:48 -0700

    feat: rewrote parser.js to be iterative

--- src/util/parser.js
+++ src/util/parser.js
@@ -1,21 +1,91 @@
-const crypto = require("crypto");
+const PARENT = Symbol("parent");
+const setParent = (input, parent) => Object.defineProperty(input, PARENT, { value: parent, writable: false });
+const getParent = input => (input[PARENT] === undefined ? null : input[PARENT]);
 
 module.exports = (input) => {
-  const tmpSep = crypto.randomBytes(16).toString('base64');
-  return input
-    // split at "." and before "[", but only if not escaped
-    .replace(/(?<!\\|^)((?:\\{2})*)(?:\.|(?=\[))(?!\[?$)/g, (_, esc) => `${esc}${tmpSep}`)
-    .split(tmpSep)
-    // handle or groups
-    .map((e) => {
-      const orMatches = [];
-      // handle basic or-groups
-      e.replace(/^{(.+)}$/g, (complete, m) => orMatches.push(...m
-        // split at ",", but only if not escaped
-        .replace(/(?<!\\|^)((?:\\{2})*)(?:,)(?!$)/g, (_, esc) => `${esc}${tmpSep}`)
-        .split(tmpSep)));
-      // handle list or-groups (only numbers)
-      e.replace(/^\[{(\d+(?:,\d+)*)}]$/g, (complete, m) => orMatches.push(...m.split(",").map(idx => `[${idx}]`)));
-      return orMatches.length === 0 ? e : orMatches;
-    });
+  if (input === "") {
+    return [""];
+  }
+
+  const result = [];
+
+  let cResult = result;
+  let inArray = false;
+  let escaped = false;
+
+  const inputLength = input.length;
+  let start = 0;
+
+  const finalizeSegment = (idx) => {
+    const segment = input.slice(start, idx);
+    if (start !== idx) {
+      if (inArray && !/^[*\d]+$/g.test(segment)) {
+        throw new Error(`Bad List Selector: ${input}, selector ${segment}`);
+      }
+      cResult.push(inArray ? `[${segment}]` : segment);
+    }
+    start = idx + 1;
+  };
+
+  for (let idx = 0; idx < inputLength; idx += 1) {
+    const char = input[idx];
+    if (escaped === false) {
+      switch (char) {
+        case ".":
+          if (idx === 0 || idx === inputLength - 1) {
+            throw new Error(`Bad Path Separator: ${input}, char ${idx}`);
+          }
+          finalizeSegment(idx);
+          break;
+        case ",":
+          if (start === idx || getParent(cResult) === null) {
+            throw new Error(`Bad Group Separator: ${input}, char ${idx}`);
+          }
+          finalizeSegment(idx);
+          break;
+        case "[":
+          if (inArray !== false) {
+            throw new Error(`Bad List Selector: ${input}, char ${idx}`);
+          }
+          finalizeSegment(idx);
+          inArray = true;
+          break;
+        case "]":
+          if (inArray !== true) {
+            throw new Error(`Bad List Selector: ${input}, char ${idx}`);
+          }
+          finalizeSegment(idx);
+          inArray = false;
+          break;
+        case "{":
+          cResult.push(setParent([], cResult));
+          cResult = cResult[cResult.length - 1];
+          start = idx + 1;
+          break;
+        case "}":
+          if (start === idx) {
+            throw new Error(`Bad Group Separator: ${input}, char ${idx - 1}`);
+          }
+          finalizeSegment(idx);
+          cResult = getParent(cResult);
+          if (cResult === null) {
+            throw new Error(`Bad Group Selector: ${input}, char ${idx}`);
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    escaped = char === "\\" ? !escaped : false;
+  }
+  if (start !== inputLength) {
+    cResult.push(input.slice(start, inputLength));
+  }
+  if (getParent(cResult) !== null) {
+    throw new Error(`Non Terminated Group Separator: ${input}`);
+  }
+  if (inArray !== false) {
+    throw new Error(`Non Terminated List Separator: ${input}`);
+  }
+  return result;
 };
--- test/util/parser.spec.js
+++ test/util/parser.spec.js
@@ -22,16 +22,12 @@ describe("Testing Parser", () => {
     expect(parser("a[0]")).to.deep.equal(["a", "[0]"]);
   });
 
-  it("Testing List Escaped", () => {
-    expect(parser("a\\[0]")).to.deep.equal(["a\\[0]"]);
-  });
-
   it("Testing Or", () => {
     expect(parser("{a,b}")).to.deep.equal([["a", "b"]]);
   });
 
   it("Testing Or Nested", () => {
-    expect(parser("{a,b.c}")).to.deep.equal(["{a,b", "c}"]);
+    expect(parser("{a,b.c}")).to.deep.equal([["a", "b", "c"]]);
   });
 
   it("Testing Or Escaped", () => {
@@ -42,35 +38,61 @@ describe("Testing Parser", () => {
     expect(parser("[{0,1}]")).to.deep.equal([["[0]", "[1]"]]);
   });
 
-  it("Testing Or In List Escaped (Invalid Group)", () => {
-    expect(parser("[{0\\,1}]")).to.deep.equal(["[{0\\,1}]"]);
-  });
-
   it("Testing List in Path", () => {
     expect(parser("a.*.c[0]")).to.deep.equal(["a", "*", "c", "[0]"]);
   });
 
-  it("Testing Starts with Dot", () => {
-    expect(parser(".a")).to.deep.equal([".a"]);
+  describe("Invalid Dot Selector", () => {
+    it("Testing Starts with Dot", () => {
+      expect(() => parser(".a")).to.throw("Bad Path Separator: .a, char 0");
+    });
+
+    it("Testing Ends with Dot", () => {
+      expect(() => parser("a.")).to.throw("Bad Path Separator: a., char 1");
+    });
   });
 
-  it("Testing Ends with Dot", () => {
-    expect(parser("a.")).to.deep.equal(["a."]);
+  describe("List Selector", () => {
+    it("Testing Invalid List Content", () => {
+      expect(() => parser("[a]")).to.throw("Bad List Selector: [a], selector a");
+    });
+
+    it("Testing Starts with Bracket", () => {
+      expect(() => parser("[a")).to.throw("Non Terminated List Separator: [a");
+    });
+
+    it("Testing Ends with Bracket", () => {
+      expect(() => parser("a]")).to.throw("Bad List Selector: a], char 1");
+    });
+
+    it("Testing Nested List Notation", () => {
+      expect(() => parser("[[")).to.throw("Bad List Selector: [[, char 1");
+    });
+
+    it("Testing Or In List Escaped (Invalid Group)", () => {
+      expect(() => parser("[{0\\,1}]")).to.throw("Bad List Selector: [{0\\,1}], selector 0\\,1");
+    });
+
+    it("Testing List Escaped", () => {
+      expect(() => parser("a\\[0]")).to.throw("Bad List Selector: a\\[0], char 4");
+    });
   });
 
-  it("Testing Starts with Bracket", () => {
-    expect(parser("[a")).to.deep.equal(["[a"]);
-  });
+  describe("Simple Group Selector", () => {
+    it("Testing Starts with Curly Bracket", () => {
+      expect(() => parser("{a")).to.throw("Non Terminated Group Separator: {a");
+    });
 
-  it("Testing Ends with Bracket", () => {
-    expect(parser("a[")).to.deep.equal(["a["]);
-  });
+    it("Testing Ends with Curly Bracket", () => {
+      expect(() => parser("a}")).to.throw("Bad Group Selector: a}, char 1");
+    });
 
-  it("Testing Group Starts with Comma", () => {
-    expect(parser("{,1,2}")).to.deep.equal([[",1", "2"]]);
-  });
+    it("Testing Group Starts with Comma", () => {
+      expect(() => parser("{,1,2}")).to.throw("Bad Group Separator: {,1,2}, char 1");
+    });
 
-  it("Testing Group Ends with Comma", () => {
-    expect(parser("{1,2,}")).to.deep.equal([["1", "2,"]]);
+    it("Testing Group Ends with Comma", () => {
+      expect(() => parser("{1,2,}")).to.throw("Bad Group Separator: {1,2,}, char 4");
+    });
   });
 });
