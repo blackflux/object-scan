b02752588f374772ab8a4b0396f8ad711c6f5d86
Jeff Soloshy <jeffsoloshy@gmail.com>
2018-12-04 15:33:59 -0800

    feat: exposed sortFn via static util attr

---
+++ src/util/sortFn.js
@@ -0,0 +1,16 @@
+module.exports = (a, b) => {
+  const lenDiff = b.length - a.length;
+  if (lenDiff !== 0) {
+    return lenDiff;
+  }
+  const diffIndex = new Array(a.length).findIndex((_, idx) => a[idx] !== b[idx]);
+  if (diffIndex !== -1) {
+    const bEle = b[diffIndex];
+    const aEle = a[diffIndex];
+    if (typeof bEle === 'number' && typeof aEle === 'number') {
+      return bEle - aEle;
+    }
+    return String(bEle).localeCompare(String(aEle));
+  }
+  return -1;
+};
