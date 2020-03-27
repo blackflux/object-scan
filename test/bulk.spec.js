acd8c94a91ef56661fb88dd6d14489ba1ab21c27
Lukas Siemon <lukas.siemon@gmail.com>
2020-03-26 19:39:00 -0700

    amend: minor

--- test/bulk.spec.js
+++ test/bulk.spec.js
@@ -3,11 +3,11 @@ const { describe } = require('node-tdd');
 const generateTests = require('./bulk/helper/generate-tests');
 const instantiateTests = require('./bulk/helper/instantiate-tests');
 
+// todo: generate keys dynamically (use function in js file or json file)
 // todo: in test generator generate combination of different intervals
 // todo: check coverage of main logic
 // todo: import released version of objectScan and compare results (?)
 // todo: profile and improve performance
-// todo: generate keys dynamically (use function in js file or json file)
 
 describe('Generating Bulk Test Coverage Tests', { useTmpDir: true }, () => {
   beforeEach(({ dir }) => {
