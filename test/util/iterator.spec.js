8b65f910df28bad91771978c2641a7b7579ff2f3
simlu <1f3ebcdbaad341a165d2827d68982d98fe92d35b>
2020-06-01 13:12:40 -0700

    amend: minor

--- test/util/iterator.spec.js
+++ test/util/iterator.spec.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const { describe } = require('node-tdd');
 const iterator = require('../../src/util/iterator');
 
-describe('Testing iterate', () => {
+describe('Testing iterator', () => {
   let treeGen;
   let recIterate;
   let visualize;
@@ -68,5 +68,25 @@ describe('Testing iterate', () => {
       iterator.iterate(data, (type, arg) => r2.push([type, type === 'FIN' ? arg.slice(0) : arg]));
       expect(r1, visualize(data)).to.deep.equal(r2);
     }
+  });
+
+  it('Testing Empty Array handling', () => {
+    const r1 = [];
+    iterator.iterate(
+      [1, new Set([2, []]), 3],
+      (type, arg) => r1.push([type, type === 'FIN' ? arg.slice(0) : arg])
+    );
+    expect(r1).to.deep.equal([
+      ['ADD', 1],
+      ['ADD', 2],
+      ['ADD', 3],
+      ['FIN', [1, 2, 3]],
+      ['RM', 3],
+      ['RM', 2],
+      ['ADD', 3],
+      ['FIN', [1, 3]],
+      ['RM', 3],
+      ['RM', 1]
+    ]);
   });
 });
