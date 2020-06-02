ece54e28710c1a1de5678a8767b1f083c77c2e5e
simlu <1f3ebcdbaad341a165d2827d68982d98fe92d35b>
2020-06-01 22:49:00 -0700

    amend: refactor

--- src/util/compiler.js
+++ src/util/compiler.js
@@ -1,5 +1,6 @@
 /* compile needles to hierarchical map object */
 const parser = require('./parser');
+const iterator = require('./iterator');
 const { defineProperty, findLast, parseWildcard } = require('./helper');
 
 const LEAF = Symbol('leaf');
@@ -55,43 +56,6 @@ module.exports.matchedBy = (searches) => extractNeedles(searches.filter((e) => i
 module.exports.excludedBy = (searches) => extractNeedles(searches.filter((e) => !isMatch(e)));
 module.exports.traversedBy = (searches) => Array.from(new Set([].concat(...searches.map((e) => getNeedles(e)))));
 
-const buildRecursive = (tower, path, ctx, excluded, root = false) => {
-  addNeedle(tower, ctx.needle);
-  if (path.length === 0) {
-    if (tower[NEEDLE] !== undefined && ctx.strict) {
-      throw new Error(`Redundant Needle Target: "${tower[NEEDLE]}" vs "${ctx.needle}"`);
-    }
-    setNeedle(tower, ctx.needle, ctx.strict);
-    markLeaf(tower, !excluded, ctx.strict);
-    if (isRecursive(tower)) {
-      setRecursionPos(tower, Object.keys(tower).length, ctx.strict);
-    }
-    return;
-  }
-  if (Array.isArray(path[0])) {
-    buildRecursive(tower, [...path[0], ...path.slice(1)], ctx, excluded);
-    return;
-  }
-  if (path[0] instanceof Set) {
-    path[0].forEach((c) => buildRecursive(tower, [c, ...path.slice(1)], ctx, excluded));
-    return;
-  }
-  if (tower[path[0]] === undefined) {
-    Object.assign(tower, { [path[0]]: {} });
-    if (String(path[0]) === '**') {
-      markRecursive(tower[path[0]]);
-    }
-    setWildcardRegex(tower[path[0]], path[0]);
-  }
-  if (excluded && path[0].isExcluded()) {
-    throw new Error(`Redundant Exclusion: "${ctx.needle}"`);
-  }
-  if (root === false && String(path[0]) === '**') {
-    buildRecursive(tower, path.slice(1), ctx, excluded || path[0].isExcluded());
-  }
-  buildRecursive(tower[path[0]], path.slice(1), ctx, excluded || path[0].isExcluded());
-};
-
 const finalizeRecursive = (tower) => {
   const towerValues = Object.values(tower);
   towerValues.forEach((v) => finalizeRecursive(v));
@@ -101,12 +65,71 @@ const finalizeRecursive = (tower) => {
   setEntries(tower, Object.entries(tower).filter(([k]) => k !== ''));
 };
 
+const iterate = (tree, needle, cb) => {
+  let excluded = false;
+  iterator.iterate(tree, (type, p) => {
+
+    if (type === 'RM') {
+      if (p.isExcluded()) {
+        excluded = false;
+      }
+    } else if (type === 'ADD') {
+      const isExcluded = p.isExcluded();
+      if (excluded && isExcluded) {
+        throw new Error(`Redundant Exclusion: "${needle}"`);
+      }
+      if (isExcluded) {
+        excluded = true;
+      }
+    }
+    cb(type, p, excluded);
+  });
+};
+
 module.exports.compile = (needles, strict = true) => {
   const tower = {};
   for (let idx = 0; idx < needles.length; idx += 1) {
     const needle = needles[idx];
-    const tree = parser.parse(needle);
-    buildRecursive(tower, [tree], { needle, strict }, false, true);
+    const tree = [parser.parse(needle)];
+    const stack = [[tower]];
+    iterate(tree, needle, (type, p, excluded) => {
+      if (type === 'RM') {
+        stack.pop();
+      } else if (type === 'ADD') {
+        const next = [];
+        stack[stack.length - 1].forEach((cur) => {
+          addNeedle(cur, needle);
+          if (cur[p] === undefined) {
+            const child = {};
+            // eslint-disable-next-line no-param-reassign
+            cur[p] = child;
+            if (String(p) === '**') {
+              markRecursive(child);
+            }
+            setWildcardRegex(child, p);
+          }
+          next.push(cur[p]);
+          if (String(p) === '**') {
+            next.push(cur);
+          }
+        });
+        stack.push(next);
+      } else {
+        stack[stack.length - 1]
+          .filter((cur) => cur !== tower || excluded)
+          .forEach((cur) => {
+            addNeedle(cur, needle);
+            if (strict && cur[NEEDLE] !== undefined) {
+              throw new Error(`Redundant Needle Target: "${cur[NEEDLE]}" vs "${needle}"`);
+            }
+            setNeedle(cur, needle, strict);
+            markLeaf(cur, !excluded, strict);
+            if (isRecursive(cur)) {
+              setRecursionPos(cur, Object.keys(cur).length, strict);
+            }
+          });
+      }
+    });
   }
   finalizeRecursive(tower);
   return tower;
