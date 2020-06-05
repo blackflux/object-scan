e342e34566b7c14d7fbeb92ccf51bae57d7db7e2
Lukas Siemon <github@blackflux.com>
2020-06-05 13:56:31 -0700

    chore: improved "generate-parsed" to include array selector

--- test/helper/generate-parsed.js
+++ test/helper/generate-parsed.js
@@ -5,6 +5,9 @@ const MAX_WIDTH = 4;
 
 const generateParsed = (depth, ctx) => {
   if (!(ctx.index < CHARS.length) || Math.random() * MAX_DEPTH < depth) {
+    if (Math.random() > 0.5) {
+      return Math.floor(Math.random() * 16);
+    }
     ctx.index += 1;
     return CHARS[(ctx.index - 1) % CHARS.length];
   }
--- test/helper/parsed-to-needle.js
+++ test/helper/parsed-to-needle.js
@@ -1,9 +1,18 @@
 const parsedToNeedle = (obj) => {
   if (Array.isArray(obj)) {
-    return `{${obj.map((e) => parsedToNeedle(e)).join('.')}}`;
+    return `{${obj.reduce((p, e) => {
+      const parsed = parsedToNeedle(e);
+      if (p === null) {
+        return parsed;
+      }
+      return parsed.startsWith('[') ? `${p}${parsed}` : `${p}.${parsed}`;
+    }, null)}}`;
   }
   if (obj instanceof Set) {
     return `{${[...obj].map((e) => parsedToNeedle(e)).join(',')}}`;
+  }
+  if (Number.isInteger(obj)) {
+    return `[${obj}]`;
   }
   return obj;
 };
