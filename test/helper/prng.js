cb3ab6c88de981bcf078f1ecdd83312eca011545
Lukas Siemon <github@blackflux.com>
2020-09-12 04:33:20 -0700

    chore: added prng for tests

---
+++ test/helper/prng.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-bitwise,no-mixed-operators,no-param-reassign */
+const xmur3 = (str) => {
+  let h = 1779033703 ^ str.length;
+  for (let i = 0; i < str.length; i += 1) {
+    h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
+    h = h << 13 | h >>> 19;
+  }
+  return () => {
+    h = Math.imul(h ^ h >>> 16, 2246822507);
+    h = Math.imul(h ^ h >>> 13, 3266489909);
+    h ^= h >>> 16;
+    return h >>> 0;
+  };
+};
+
+function sfc32(a, b, c, d) {
+  return () => {
+    a >>>= 0;
+    b >>>= 0;
+    c >>>= 0;
+    d >>>= 0;
+    let t = (a + b) | 0;
+    a = b ^ b >>> 9;
+    b = c + (c << 3) | 0;
+    c = (c << 21 | c >>> 11);
+    d = d + 1 | 0;
+    t = t + d | 0;
+    c = c + t | 0;
+    return (t >>> 0) / 4294967296;
+  };
+}
+
+module.exports = (str) => {
+  const seed = xmur3(str);
+  return sfc32(seed(), seed(), seed(), seed());
+};
