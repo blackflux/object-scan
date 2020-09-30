11f6f41b986b64168709c0530aae9ec8d407450e
Lukas Siemon <github@blackflux.com>
2020-09-30 04:09:07 -0700

    amend: rename

--- test/helper/extract-paths-from-haystack.js
+++ test/helper/extract-paths-from-haystack.js
@@ -1,15 +1,15 @@
-const extractNeedlesRec = (haystack, path = []) => {
+const extractPathsRec = (haystack, path = []) => {
   const result = [];
   if (Array.isArray(haystack)) {
     haystack
       .forEach((value, key) => {
-        result.push(...extractNeedlesRec(value, path.concat(key)));
+        result.push(...extractPathsRec(value, path.concat(key)));
       });
   } else if (haystack instanceof Object) {
     Object
       .entries(haystack)
       .forEach(([key, value]) => {
-        result.push(...extractNeedlesRec(value, path.concat(key)));
+        result.push(...extractPathsRec(value, path.concat(key)));
       });
   } else {
     result.push(path);
@@ -17,4 +17,4 @@ const extractNeedlesRec = (haystack, path = []) => {
   return result;
 };
 
-module.exports = (haystack) => extractNeedlesRec(haystack);
+module.exports = (haystack) => extractPathsRec(haystack);
