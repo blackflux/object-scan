5b9f0258a4682258598e01bbcfc4fe1d0f2a56e5
Lukas Siemon <github@blackflux.com>
2020-10-02 04:28:39 -0700

    chore: minor improvements to parsedNeedleToString

--- test/helper/parsed-needle-to-string.js
+++ test/helper/parsed-needle-to-string.js
@@ -1,15 +1,33 @@
 const parsedNeedleToString = (obj) => {
   if (Array.isArray(obj)) {
-    return `{${obj.reduce((p, e) => {
+    const len = obj.length;
+    if (len === 0) {
+      return null;
+    }
+    return `${
+      len === 1 ? '' : '{'
+    }${obj.reduce((p, e) => {
       const parsed = parsedNeedleToString(e);
       if (p === null) {
         return parsed;
       }
       return parsed.startsWith('[') ? `${p}${parsed}` : `${p}.${parsed}`;
-    }, null)}}`;
+    }, null)}${
+      len === 1 ? '' : '}'
+    }`;
   }
   if (obj instanceof Set) {
-    return `{${[...obj].map((e) => parsedNeedleToString(e)).join(',')}}`;
+    const size = obj.size;
+    if (size === 0) {
+      return null;
+    }
+    return `${
+      size === 1 ? '' : '{'
+    }${
+      [...obj].map((e) => parsedNeedleToString(e)).join(',')
+    }${
+      size === 1 ? '' : '}'
+    }`;
   }
   return obj;
 };
