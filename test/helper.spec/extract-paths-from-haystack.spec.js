cd912f241350f080dc73c80232a4d6720aa14a9e
Lukas Siemon <github@blackflux.com>
2020-10-03 13:24:32 -0700

    amend: minor refactor

--- test/helper.spec/extract-paths-from-haystack.spec.js
+++ test/helper.spec/extract-paths-from-haystack.spec.js
@@ -1,16 +1,10 @@
 const expect = require('chai').expect;
 const { describe } = require('node-tdd');
-const PRNG = require('../helper/prng');
-const generateHaystack = require('../helper/generate-haystack');
-const haystackGenerator = require('../helper/haystack-generator');
 const extractPathsFromHaystack = require('../helper/extract-paths-from-haystack');
 
 describe('Testing extract-paths-from-haystack.js', () => {
-  it('Testing example', () => {
-    const rng = PRNG('cb8c58e7-436d-4229-8ad5-eca7670fb803');
-    const generator = haystackGenerator({ rng });
-    const haystack = generateHaystack(generator);
-    expect(haystack).to.deep.equal({ a: [{ '&': 0, f: [1, { F: 2, ',': 3, ';': 4 }] }] });
+  it('Testing with useArraySelector', () => {
+    const haystack = { a: [{ '&': 0, f: [1, { F: 2, ',': 3, ';': 4 }] }] };
     const needles = extractPathsFromHaystack(haystack);
     expect(needles).to.deep.equal([
       ['a', 0, '&'],
