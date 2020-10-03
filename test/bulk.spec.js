cd968181bccf4ea9ee50404b4a8abd35f067673c
Lukas Siemon <github@blackflux.com>
2020-10-03 10:14:49 -0700

    chore: now using random words for bulk test

--- test/bulk.spec.js
+++ test/bulk.spec.js
@@ -11,21 +11,50 @@ const pathToNeedlePath = require('./helper/path-to-needle-path');
 const parsedNeedleToString = require('./helper/parsed-needle-to-string');
 const objectScan = require('../src/index');
 
-describe('Testing bulk related', () => {
-  it('Testing all needles matched', () => {
-    for (let idx = 0; idx < 50; idx += 1) {
-      const seed = `${Math.random()}`;
-      const rng = PRNG(seed);
+describe('Testing bulk related', { timeout: 5 * 60000 }, () => {
+  let init;
+  beforeEach(() => {
+    init = () => {
+      const rng = PRNG(`${Math.random()}`);
       const keys = generateKeys(Math.ceil(rng() * 30), rng);
       const haystack = generateHaystack(haystackGenerator({ rng, keys }));
       const paths = extractPathsFromHaystack(haystack);
       const pathsShuffled = [...paths];
       shuffleArray(pathsShuffled, rng);
+      return {
+        rng, haystack, paths, pathsShuffled
+      };
+    };
+  });
+
+  it('Testing simple matching', () => {
+    for (let idx = 0; idx < 50; idx += 1) {
+      const {
+        rng, haystack, paths, pathsShuffled
+      } = init();
       const needlePaths = pathsShuffled.map((p) => pathToNeedlePath(p));
       const needles = needlePathsToNeedlesParsed(needlePaths);
       const str = parsedNeedleToString(needles);
       const matches = objectScan(str === null ? [] : [str])(haystack);
-      expect(paths, `Seed: ${seed}`).to.deep.equal(matches.reverse());
+      expect(paths, `Seed: ${rng.seed}`).to.deep.equal(matches.reverse());
+    }
+  });
+
+  it('Testing parameter matching', () => {
+    for (let idx = 0; idx < 50; idx += 1) {
+      const {
+        rng, haystack, paths, pathsShuffled
+      } = init();
+      const needlePaths = pathsShuffled.map((p) => pathToNeedlePath(p, {
+        questionMark: rng() > 0.2 ? 0 : Math.floor(rng() * p.length) + 1,
+        partialStar: rng() > 0.2 ? 0 : Math.floor(rng() * p.length) + 1,
+        singleStar: rng() > 0.2 ? 0 : Math.floor(rng() * p.length) + 1,
+        doubleStar: rng() > 0.2 ? 0 : Math.floor(rng() * p.length) + 1
+      }, rng));
+      const needles = needlePathsToNeedlesParsed(needlePaths);
+      const str = parsedNeedleToString(needles);
+      const matches = objectScan(str === null ? [] : [str], { strict: false })(haystack);
+      expect(matches, `Seed: ${rng.seed}`).to.include.deep.members(paths);
     }
   });
 });
--- test/helper.spec/generate-keys.spec.js
+++ test/helper.spec/generate-keys.spec.js
@@ -1,13 +1,19 @@
 const expect = require('chai').expect;
 const { describe } = require('node-tdd');
+const PRNG = require('../helper/prng');
 const generateKeys = require('../helper/generate-keys');
 
 describe('Testing generate-keys.js', { cryptoSeed: 'c9bfe12c-329a-4d3a-afb7-71fbca2cb77b' }, () => {
   it('Testing example', () => {
-    const r = generateKeys(10);
+    const r = generateKeys(10, PRNG('b6b13c0a-7f68-4ca9-99e8-908ab6159fb0'));
     expect(r).to.deep.equal([
-      'ysuCl', 'was', 'bf1[<all', 'running', 'wild',
-      'harder', 'road', 'y~{2Z|;U:', '<i>.', 'axerkS~ia1e'
+      'heLb', 'within', 'sport', 'stay', 'rjutT',
+      'mud', 'w&od', 'pGon', 'explore', 'd*tUil'
     ]);
+  });
+
+  it('Testing default rng', () => {
+    const r = generateKeys(1);
+    expect(r).to.deep.equal(['shall']);
   });
 });
