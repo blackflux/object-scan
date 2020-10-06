3d9d8ebc9014483243aedb09927d8593869968a9
Lukas Siemon <github@blackflux.com>
2020-10-06 05:31:52 -0700

    chore: generate-dataset now takes optional seed

--- test/helper/generate-dataset.js
+++ test/helper/generate-dataset.js
@@ -4,8 +4,8 @@ const generateKeys = require('./generate-keys');
 const generateHaystack = require('./generate-haystack');
 const extractPathsFromHaystack = require('./extract-paths-from-haystack');
 
-module.exports = () => {
-  const rng = PRNG(`${Math.random()}`);
+module.exports = (seed = null) => {
+  const rng = PRNG(seed === null ? `${Math.random()}` : seed);
   const keys = generateKeys(Math.ceil(rng() * 30), rng);
   const haystack = generateHaystack(haystackGenerator({ rng, keys }));
   const paths = extractPathsFromHaystack(haystack);
