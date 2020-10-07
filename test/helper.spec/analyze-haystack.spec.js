144fe78749b283a9f0e2882f04ea45dd61ff60fd
Lukas Siemon <github@blackflux.com>
2020-10-07 04:02:45 -0700

    amend: improved analyze-haystack

--- test/helper.spec/analyze-haystack.spec.js
+++ test/helper.spec/analyze-haystack.spec.js
@@ -5,16 +5,34 @@ const analyzeHaystack = require('../helper/analyze-haystack');
 describe('Testing analyze-haystack.js', () => {
   it('Testing basic', () => {
     const r = analyzeHaystack({ parent: [{ v: 1 }, { v: 2 }] });
-    expect(r).to.deep.equal({ branchingFactor: 1.25, depth: 3, leaves: 2 });
+    expect(r).to.deep.equal({
+      branchingFactor: 1.25,
+      depth: 3,
+      leaves: 2,
+      hasArray: true,
+      hasObject: true
+    });
   });
 
   it('Testing empty object', () => {
     const r = analyzeHaystack({});
-    expect(r).to.deep.equal({ branchingFactor: 0, depth: 0, leaves: 0 });
+    expect(r).to.deep.equal({
+      branchingFactor: 0,
+      depth: 0,
+      leaves: 0,
+      hasArray: false,
+      hasObject: true
+    });
   });
 
   it('Testing empty array', () => {
-    const r = analyzeHaystack({});
-    expect(r).to.deep.equal({ branchingFactor: 0, depth: 0, leaves: 0 });
+    const r = analyzeHaystack([]);
+    expect(r).to.deep.equal({
+      branchingFactor: 0,
+      depth: 0,
+      leaves: 0,
+      hasArray: true,
+      hasObject: false
+    });
   });
 });
--- test/helper/analyze-haystack.js
+++ test/helper/analyze-haystack.js
@@ -2,12 +2,14 @@ const findDepth = (obj, depth, ctx) => {
   if (Array.isArray(obj)) {
     ctx.nodes += 1;
     ctx.branches += obj.length;
+    ctx.hasArray = true;
     return obj.reduce((p, c) => Math.max(p, findDepth(c, depth + 1, ctx)), depth);
   }
   if (obj instanceof Object) {
     const values = Object.values(obj);
     ctx.nodes += 1;
     ctx.branches += values.length;
+    ctx.hasObject = true;
     return values.reduce((p, c) => Math.max(p, findDepth(c, depth + 1, ctx)), depth);
   }
   ctx.leaves += 1;
@@ -15,11 +17,19 @@ const findDepth = (obj, depth, ctx) => {
 };
 
 module.exports = (obj) => {
-  const ctx = { leaves: 0, nodes: 0, branches: 0 };
+  const ctx = {
+    leaves: 0,
+    nodes: 0,
+    branches: 0,
+    hasArray: false,
+    hasObject: false
+  };
   const depth = findDepth(obj, 0, ctx);
   return {
     depth,
     leaves: ctx.leaves,
-    branchingFactor: ctx.branches / ctx.nodes
+    branchingFactor: ctx.branches / ctx.nodes,
+    hasArray: ctx.hasArray,
+    hasObject: ctx.hasObject
   };
 };
