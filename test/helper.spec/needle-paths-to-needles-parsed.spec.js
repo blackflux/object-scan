de22071d731184c8079602213581672780e7ade5
Lukas Siemon <github@blackflux.com>
2020-10-18 16:53:18 -0700

    amend: minor

--- test/helper.spec/needle-paths-to-needles-parsed.spec.js
+++ test/helper.spec/needle-paths-to-needles-parsed.spec.js
@@ -237,7 +237,7 @@ describe('Testing needle-paths-to-needles-parsed.js', { timeout: 5000 }, () => {
         partialStar: rng() > 0.2 ? 0 : Math.floor(rng() * p.length) + 1,
         singleStar: rng() > 0.2 ? 0 : Math.floor(rng() * p.length) + 1,
         doubleStar: rng() > 0.2 ? 0 : Math.floor(rng() * p.length) + 1,
-        regex: rng() > 0.2 ? 0 : Math.floor(rng() * p.length) + 1,
+        regex: rng() > 0.1 ? 0 : Math.floor(rng() * p.length) + 1,
         exclude: rng() > 0.9,
         shuffle: rng() > 0.9
       }, rng));
