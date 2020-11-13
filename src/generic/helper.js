ef41060f064069e89d1707298f59a489c709f943
Lukas Siemon <github@blackflux.com>
2020-11-12 17:43:23 -0800

    chore: refactor

--- src/generic/helper.js
+++ src/generic/helper.js
@@ -5,17 +5,15 @@ const specialChars = /[?!,.*+[\](){}\\]/g;
 const escape = (input) => input.replace(specialChars, '\\$&');
 module.exports.escape = escape;
 
-const escapeRegex = (char) => char.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
-module.exports.escapeRegex = escapeRegex;
+module.exports.escapeRegex = (char) => char.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
 
-const asRegex = (regexStr) => {
+module.exports.asRegex = (regexStr) => {
   try {
     return new RegExp(regexStr);
   } catch (e) {
     throw new Error(`Invalid Regex: "${regexStr}"`);
   }
 };
-module.exports.asRegex = asRegex;
 
 module.exports.toPath = (input) => input
   .reduce((p, c) => `${p}${typeof c === 'number' ? `[${c}]` : `${p ? '.' : ''}${escape(c)}`}`, '');
