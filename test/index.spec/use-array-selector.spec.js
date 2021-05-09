18723f578c723635b4683d1df4a443d677ef1ae1
Lukas Siemon <github@blackflux.com>
2021-05-09 13:46:28 -0700

    amend: minor

--- test/index.spec/use-array-selector.spec.js
+++ test/index.spec/use-array-selector.spec.js
@@ -59,5 +59,9 @@ describe('Testing useArraySelector', () => {
         ['[1][1].a', '[1][1]', '[1][0].b', '[1][0]', '[0][0].b', '[0][0].a', '[0][0]']
       );
     });
+
+    it('Testing root match negated by star recursion root match', () => {
+      tester([0, 1], ['', '!**'], []);
+    });
   });
 });
