fd456ac66d1a5ea130a3b11dff98df5f6cb8947a
Lukas Siemon <lukas.siemon@blackflux.com>
2022-02-10 08:33:27 -0800

    amend: minor

--- test/helper/generate-dataset.js
+++ test/helper/generate-dataset.js
@@ -1,11 +1,9 @@
-import { v4 } from 'uuid';
+import { v4 as uuid } from 'uuid';
 import PRNG from './prng.js';
 import haystackGenerator from './haystack-generator.js';
 import generateKeys from './generate-keys.js';
 import generateHaystack from './generate-haystack.js';
 import extractPathsFromHaystack from './extract-paths-from-haystack.js';
-
-const uuid = v4;
 
 export default (seed = null) => {
   const rng = PRNG(seed === null ? uuid() : seed);
