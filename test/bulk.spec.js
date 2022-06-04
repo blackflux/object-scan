155c481e8e825a353fd83ff5c21a79720e37c6ef
Lukas Siemon <lukas.siemon@blackflux.com>
2022-06-04 10:08:00 -0700

    amend: todo

--- test/bulk.spec.js
+++ test/bulk.spec.js
@@ -76,6 +76,9 @@ const Tester = () => {
 
       expect(breakMatchedKeys.sort(), `Seed: ${rng.seed}`).to.deep.equal(filterMatchedKeys.sort());
       expect(!breakNonMatchedKeys.some((e) => filterMatchedKeys.includes(e)), `Seed: ${rng.seed}`).to.equal(true);
+
+      // todo: add check to ensure needles are always returned "upwards"
+      // ...
     }
   };
 };
