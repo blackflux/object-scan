01becbd5c488ee8a0875250bde208e6e630816ce
Lukas Siemon <lukas.siemon@blackflux.com>
2022-06-05 18:59:08 -0700

    amend: test case with todo

--- test/index.spec.js
+++ test/index.spec.js
@@ -923,14 +923,14 @@ describe('Testing Find', () => {
     const tree = { f: { b: { a: {}, d: { c: {}, e: {} } }, g: { i: { h: {} } } } };
     expect(objectScan(['**'], options)(tree, { filterFn: [], breakFn: [] }))
       .to.deep.equal({
-        // Post-order reverse
+      // Post-order reverse
         breakFn: [undefined, 'f', 'g', 'i', 'h', 'b', 'd', 'e', 'c', 'a'],
         // Pre-order reverse
         filterFn: ['h', 'i', 'g', 'e', 'c', 'd', 'a', 'b', 'f']
       });
     expect(objectScan(['**'], { ...options, reverse: false })(tree, { filterFn: [], breakFn: [] }))
       .to.deep.equal({
-        // Pre-order
+      // Pre-order
         breakFn: [undefined, 'f', 'b', 'a', 'd', 'c', 'e', 'g', 'i', 'h'],
         // Post-order
         filterFn: ['a', 'c', 'e', 'd', 'b', 'h', 'i', 'g', 'f']
@@ -1442,6 +1442,18 @@ describe('Testing Find', () => {
         'a.a.a',
         'a.a',
         'a'
+      ]);
+    });
+
+    it('Testing starstar in recursive group matches skip', () => {
+      // todo: this should include "a.a.a.a.a.a.a.a.a.a"
+      expect(t('a.a.a.a.a.a.a.a.a.a.++{**}')).to.deep.equal([
+        'a.a.a.a.a.a.a.a.a.a.nn',
+        'a.a.a.a.a.a.a.a.a.a.n',
+        'a.a.a.a.a.a.a.a.a.a.d',
+        'a.a.a.a.a.a.a.a.a.a.c',
+        'a.a.a.a.a.a.a.a.a.a.b',
+        'a.a.a.a.a.a.a.a.a.a.a'
       ]);
     });
   });
