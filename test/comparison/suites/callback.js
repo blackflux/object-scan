1a74bf0ceadb733dd8c5c3224ff325a0d423b52b
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-17 11:07:03 -0700

    amend: minor

---
+++ test/comparison/suites/callback.js
@@ -0,0 +1,152 @@
+import { JSONPath } from 'jsonpath-plus';
+import objectScan from '../../../src/index.js';
+
+const objScanResult = [{
+  key: ['F', 'G', 'I', 'H'],
+  value: 3,
+  parent: { H: 3 },
+  property: 'H'
+}, {
+  key: ['F', 'G', 'I'],
+  value: { H: 3 },
+  parent: { I: { H: 3 } },
+  property: 'I'
+}, {
+  key: ['F', 'G'],
+  value: { I: { H: 3 } },
+  parent: { B: { A: 0, D: { C: 1, E: 2 } }, G: { I: { H: 3 } } },
+  property: 'G'
+}, {
+  key: ['F', 'B', 'D', 'E'],
+  value: 2,
+  parent: { C: 1, E: 2 },
+  property: 'E'
+}, {
+  key: ['F', 'B', 'D', 'C'],
+  value: 1,
+  parent: { C: 1, E: 2 },
+  property: 'C'
+}, {
+  key: ['F', 'B', 'D'],
+  value: { C: 1, E: 2 },
+  parent: { A: 0, D: { C: 1, E: 2 } },
+  property: 'D'
+}, {
+  key: ['F', 'B', 'A'],
+  value: 0,
+  parent: { A: 0, D: { C: 1, E: 2 } },
+  property: 'A'
+}, {
+  key: ['F', 'B'],
+  value: { A: 0, D: { C: 1, E: 2 } },
+  parent: { B: { A: 0, D: { C: 1, E: 2 } }, G: { I: { H: 3 } } },
+  property: 'B'
+}, {
+  key: ['F'],
+  value: { B: { A: 0, D: { C: 1, E: 2 } }, G: { I: { H: 3 } } },
+  parent: { F: { B: { A: 0, D: { C: 1, E: 2 } }, G: { I: { H: 3 } } } },
+  property: 'F'
+}];
+
+export default {
+  _index: 4,
+  _name: 'Callback',
+  _fixture: 'tree',
+  objectScanCompiled: {
+    fn: objectScan(['**'], {
+      beforeFn: (state) => {
+        // eslint-disable-next-line no-param-reassign
+        state.context = [];
+      },
+      filterFn: ({
+        context, key, value, parent, property
+      }) => {
+        context.push({
+          key, value, parent, property
+        });
+      },
+      rtn: 'context'
+    }),
+    result: objScanResult
+  },
+  objectScan: {
+    fn: (v) => objectScan(['**'], {
+      filterFn: ({
+        context, key, value, parent, property
+      }) => {
+        context.push({
+          key, value, parent, property
+        });
+      }
+    })(v, []),
+    result: objScanResult
+  },
+  jsonpathplus: {
+    fn: (v) => {
+      const result = [];
+      JSONPath({
+        path: '$..[*]',
+        json: v,
+        callback: (value, resultType, context) => {
+          result.push(context);
+        }
+      });
+      return result;
+    },
+    result: [{
+      path: "$['F']",
+      value: { B: { A: 0, D: { C: 1, E: 2 } }, G: { I: { H: 3 } } },
+      parent: { F: { B: { A: 0, D: { C: 1, E: 2 } }, G: { I: { H: 3 } } } },
+      parentProperty: 'F',
+      hasArrExpr: true
+    }, {
+      path: "$['F']['B']",
+      value: { A: 0, D: { C: 1, E: 2 } },
+      parent: { B: { A: 0, D: { C: 1, E: 2 } }, G: { I: { H: 3 } } },
+      parentProperty: 'B',
+      hasArrExpr: true
+    }, {
+      path: "$['F']['G']",
+      value: { I: { H: 3 } },
+      parent: { B: { A: 0, D: { C: 1, E: 2 } }, G: { I: { H: 3 } } },
+      parentProperty: 'G',
+      hasArrExpr: true
+    }, {
+      path: "$['F']['B']['A']",
+      value: 0,
+      parent: { A: 0, D: { C: 1, E: 2 } },
+      parentProperty: 'A',
+      hasArrExpr: true
+    }, {
+      path: "$['F']['B']['D']",
+      value: { C: 1, E: 2 },
+      parent: { A: 0, D: { C: 1, E: 2 } },
+      parentProperty: 'D',
+      hasArrExpr: true
+    }, {
+      path: "$['F']['B']['D']['C']",
+      value: 1,
+      parent: { C: 1, E: 2 },
+      parentProperty: 'C',
+      hasArrExpr: true
+    }, {
+      path: "$['F']['B']['D']['E']",
+      value: 2,
+      parent: { C: 1, E: 2 },
+      parentProperty: 'E',
+      hasArrExpr: true
+    }, {
+      path: "$['F']['G']['I']",
+      value: { H: 3 },
+      parent: { I: { H: 3 } },
+      parentProperty: 'I',
+      hasArrExpr: true
+    }, {
+      path: "$['F']['G']['I']['H']",
+      value: 3,
+      parent: { H: 3 },
+      parentProperty: 'H',
+      hasArrExpr: true
+    }]
+  }
+};
