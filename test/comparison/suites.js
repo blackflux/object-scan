640d1c1b9ad954d6c06f00b966a25405c9b5dd74
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-19 23:17:05 -0700

    amend: added wildcard suite

--- test/comparison/suites.js
+++ test/comparison/suites.js
@@ -5,6 +5,7 @@ import value from './suites/value.js';
 import callback from './suites/callback.js';
 import parent from './suites/parent.js';
 import multiplePaths from './suites/multiple-paths.js';
+import wildcard from './suites/wildcard.js';
 import regex from './suites/regex.js';
 import exclusion from './suites/exclusion.js';
 import pathRecursion from './suites/path-recursion.js';
@@ -17,6 +18,7 @@ export default {
   callback,
   parent,
   multiplePaths,
+  wildcard,
   regex,
   exclusion,
   pathRecursion
