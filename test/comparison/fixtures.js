2300f8b7aab5b71740f5a40b363cd96bb93df801
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-19 22:49:44 -0700

    amend: added exclusion and path recursion to suites

--- test/comparison/fixtures.js
+++ test/comparison/fixtures.js
@@ -1,2 +1,3 @@
 export { default as cond } from './fixtures/cond.js';
 export { default as tree } from './fixtures/tree.js';
+export { default as nested } from './fixtures/nested.js';
--- test/comparison/suites.js
+++ test/comparison/suites.js
@@ -5,6 +5,8 @@ import value from './suites/value.js';
 import callback from './suites/callback.js';
 import parent from './suites/parent.js';
 import multiplePaths from './suites/multiple-paths.js';
+import exclusion from './suites/exclusion.js';
+import pathRecursion from './suites/path-recursion.js';
 
 export default {
   key,
@@ -13,5 +15,7 @@ export default {
   recursive,
   callback,
   parent,
-  multiplePaths
+  multiplePaths,
+  exclusion,
+  pathRecursion
 };
