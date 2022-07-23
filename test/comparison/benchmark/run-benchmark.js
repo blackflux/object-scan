eb438ef63c5d7a2042746ed7dc69f6232262fcb8
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-22 22:45:20 -0700

    amend: adjusted benchmark generation

--- test/comparison/benchmark/run-benchmark.js
+++ test/comparison/benchmark/run-benchmark.js
@@ -1,13 +1,17 @@
 import * as fixtures from '../fixtures.js';
 
 const COUNT = 1000;
+const trim = 0.25;
 
-// todo: discard outlier
 export default (fn, fixture) => {
-  const start = process.hrtime();
+  const times = Array(COUNT);
   for (let k = 0; k < COUNT; k += 1) {
+    const start = process.hrtime();
     fn(fixtures[fixture]);
+    const stop = process.hrtime(start);
+    times[k] = stop[0] * 1e9 + stop[1];
   }
-  const stop = process.hrtime(start);
-  return (stop[0] * 1e9 + stop[1]) / (COUNT * 1000);
+  times.sort();
+  const timesRelevant = times.slice(COUNT * trim, COUNT * (1 - trim));
+  return timesRelevant.reduce((a, b) => a + b, 0) / (timesRelevant.length * 1000.0);
 };
