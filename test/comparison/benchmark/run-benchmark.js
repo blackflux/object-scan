6b8fd107d89ae840a628e43fc22fbf99969d3fce
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-22 21:45:24 -0700

    amend: run-benchmark + test

---
+++ test/comparison/benchmark/run-benchmark.js
@@ -0,0 +1,12 @@
+import * as fixtures from '../fixtures.js';
+
+const COUNT = 1000;
+
+export default (fn, fixture) => {
+  const start = process.hrtime();
+  for (let k = 0; k < COUNT; k += 1) {
+    fn(fixtures[fixture]);
+  }
+  const stop = process.hrtime(start);
+  return (stop[0] * 1e9 + stop[1]) / (COUNT * 1000);
+};
