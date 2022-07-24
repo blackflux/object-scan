d52ace287884b6d4f971c4b927f535816cd2fa98
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-24 12:05:19 -0700

    amend: refactor

--- test/readme/verify-links/verify-refs.js
+++ test/readme/verify-links/verify-refs.js
@@ -1,10 +1,19 @@
 import assert from 'assert';
+import Slugger from 'github-slugger';
 
 export default async (refs, content) => {
+  const slugger = new Slugger();
+
+  const headerSlugs = content
+    .match(/(?<=##+ )[^\n]+/g)
+    .map((e) => slugger.slug(e, false));
+
   for (let i = 0; i < refs.length; i += 1) {
     const ref = refs[i];
-    // check this is not a auto generated heading slug
-    if (!/^#\d+-/.test(ref)) {
+    if (/^#\d+-/.test(ref)) {
+      // ensure this is (probably) a valid header slug
+      assert(headerSlugs.includes(ref.slice(1)), ref);
+    } else {
       // ensuring id is declared somewhere in document
       assert(content.includes(` id="${ref.slice(1)}"`), ref);
     }
